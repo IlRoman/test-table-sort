1fcbd2e96d4b40e09c1dc9092cbf17a7b1345826
“ilroman” <il.roman@ukr.net>
2020-07-18 12:08:02 +0300

    Updates

---
+++ precache-manifest.d35f23632bd2f353cc64255f160f0749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688d855468017ab04e6e3ba8530bd132",
+    "url": "/test-table-sort/index.html"
+  },
+  {
+    "revision": "5822c0650d59f18109cf",
+    "url": "/test-table-sort/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "b3333ee21cc985629f6c",
+    "url": "/test-table-sort/static/js/2.781b8f5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-table-sort/static/js/2.781b8f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5822c0650d59f18109cf",
+    "url": "/test-table-sort/static/js/main.a5be9ccd.chunk.js"
+  },
+  {
+    "revision": "479882dcbbcd0842460a",
+    "url": "/test-table-sort/static/js/runtime-main.74d132d2.js"
+  }
+]);
